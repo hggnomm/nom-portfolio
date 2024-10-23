4460475dda61c86211ad5c2d1debd9978f4d88a3
hggnomm <hggnomm.dev@gmail.com>
2024-10-24 01:36:54 +0700

    commit first

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ['selector', '[data-mode="dark"]'],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,6 +10,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        mainBackground: '#222831',
+        whiteColor: '#F2F2F2',
+        grayColor: '#393E46',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
