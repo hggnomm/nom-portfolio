72cb0d0cf3f1114ca706a8e8e52ad976d02d4f59
hggnomm <hggnomm.dev@gmail.com>
2025-01-04 10:57:11 +0700

    next-intl multi language for SEO

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,16 @@
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
+import { getTranslations } from "next-intl/server";
+
+export default function HomePage() {
+  // server request async stuff ==> below code
+  // const t = await getTranslations("Homepage")
+
+  const t = useTranslations("HomePage");
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <h1>{t("about")}</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function HomePage() {
-  return <h1 className="text-secondary-color dark:text-primary-color">Nam Dep Trai</h1>;
-}
