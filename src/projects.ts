261542d38ea12bbb2de50ee73c721f7e2c0f0541
hggnomm <hggnomm.dev@gmail.com>
2025-02-16 00:28:14 +0700

    update

--- src/projects.ts
+++ src/projects.ts
@@ -17,6 +17,8 @@ export const projects = [
       "mySQL",
       "AWS",
       "Docker",
+      "Nginx",
+      "Github Action",
     ],
   },
   {
@@ -26,44 +28,38 @@ export const projects = [
     image: "/images/projects/katenai-shield/preview.png",
     description:
       "A cross-platform mobile project on the topic of domestic violence, suitable for all users with an easy-to-see and eye-catching interface. Currently only the UI part is available",
+    stacks: ["TypeScript", "React Native", "Figma", "Expo", "Google Maps API"],
+  },
+  {
+    id: "osmd",
+    title: "OSMD",
+    timeline: "2024",
+    image: "/images/projects/osmd/preview.png",
+    description:
+      "A project for my piano playing. It converts PDF files to MusicXML, reads MusicXML files to render music notes, and plays music online.",
     stacks: [
       "TypeScript",
-      "React Native",
-      "Figma",
-      "Expo",
-      "Google Maps API",
+      "React.js",
+      "ASP.NET",
+      "Vite.js",
+      "opensheetmusicdisplay",
+      "osmd",
     ],
   },
-  // {
-  //   id: "osmd",
-  //   title: "OSMD",
-  //   timeline: "2023 - Current",
-  //   image: "/images/projects/osmd/preview.png",
-  //   description:
-  //     "A project for my piano playing. It converts PDF files to MusicXML, reads MusicXML files to render music notes, and plays music online.",
-  //   stacks: [
-  //     "TypeScript",
-  //     "React.js",
-  //     "ASP.NET",
-  //     "Vite.js",
-  //     "opensheetmusicdisplay",
-  //     "osmd",
-  //   ],
-  // },
-  // {
-  //   id: "wubi-music",
-  //   title: "Wubi Music",
-  //   timeline: "2022",
-  //   image: "/images/projects/wubi/preview.png",
-  //   description:
-  //     "A small project in college. A music listening website, authenticated with google account, beautiful page, and smooth effects.",
-  //   stacks: [
-  //     "React.js",
-  //     "Express.js",
-  //     "TailwindCSS",
-  //     "Notion Framer",
-  //     "MongoDB",
-  //     "Firebase",
-  //   ],
-  // },
+  {
+    id: "wubi-music",
+    title: "Wubi Music",
+    timeline: "2022",
+    image: "/images/projects/wubi/preview.png",
+    description:
+      "A small project in college. A music listening website, authenticated with google account, beautiful page, and smooth effects.",
+    stacks: [
+      "React.js",
+      "Express.js",
+      "TailwindCSS",
+      "Notion Framer",
+      "MongoDB",
+      "Firebase",
+    ],
+  },
 ];
